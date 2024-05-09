6bdb0fb4289864e4e1aea30ddad8457ea6d0eaba
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-05-08 20:56:56 -0400

    daily

--- cypress/e2e/main-page.cy.js
+++ cypress/e2e/main-page.cy.js
@@ -4,5 +4,6 @@ describe("Open todoMVC web page", () => {
   it("open the website main page", () => {
     cy.visit("https://todomvc.com/examples/javascript-es6/dist/");
     mainPage.inputText();
+  
   });
 });
